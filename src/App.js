5d31bad8007a7f930eb533fba202e1dd571d3208
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-05-26 21:54:07 +0200

    feat: hidden prop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React, { useEffect, useState } from "react";
-import { Col, Row, Normalize, Grid, Typography } from "@smooth-ui/core-sc";
+import {
+  Col,
+  Row,
+  Normalize,
+  Grid,
+  Typography,
+  Button
+} from "@smooth-ui/core-sc";
 
 import Pokemon from "./components/Pokemon";
 import SearchInput from "./components/SeachInput";
-
 import PokeData from "./containers/PokeData";
 
+export function sleep(duration) {
+  return new Promise(resolve => {
+    setTimeout(resolve, duration);
+  });
+}
+
+const Tab = ({ hidden, children }) => <div hidden={hidden}>{children}</div>;
+
+const ExpensiveTree = React.lazy(async () => {
+  await sleep(1000);
+  return import("./containers/Expensive");
+});
+
+function PokemonTree({ hidden }) {
+  const [query, changeQuery] = useState("fire");
+
+  return (
+    <>
+      <Typography variant="display-1">Pokemon Types</Typography>
+      <SearchInput value={query} onChange={e => changeQuery(e.target.value)} />
+      <p>Search: {query}</p>
+      <Row>
+        <PokeData query={query}>
+          {({ pokemon }) =>
+            pokemon.slice(0, 25).map(({ pokemon: { name, ...rest } }) => (
+              <Col key={name} my={1} xs={12} md={6}>
+                <Pokemon {...rest} name={name} />
+              </Col>
+            ))
+          }
+        </PokeData>
+      </Row>
+    </>
+  );
+}
+
 function App() {
-  const [query, changeQuery] = useState("");
+  const [active, setActive] = useState(0);
 
   useEffect(() => {
     document.title = "Workshop - React Europe";
@@ -17,23 +59,22 @@ function App() {
     <>
       <Normalize />
       <Grid py={20}>
-        <Typography variant="display-1">Pokemon Types</Typography>
-        <SearchInput
-          value={query}
-          onChange={e => changeQuery(e.target.value)}
-        />
-        <p>Search: {query}</p>
-        <Row>
-          <PokeData query={query}>
-            {({ pokemon }) =>
-              pokemon.slice(0, 25).map(({ pokemon: { name, ...rest } }) => (
-                <Col key={name} my={1} xs={12} md={6}>
-                  <Pokemon {...rest} name={name} />
-                </Col>
-              ))
-            }
-          </PokeData>
-        </Row>
+        <>
+          <Button onClick={() => setActive(0)}>First</Button>
+          <Button onClick={() => setActive(1)}>Second</Button>
+          <Button onClick={() => setActive(2)}>Third</Button>
+        </>
+        <React.Suspense fallback={<div>Loading</div>}>
+          <Tab hidden={active !== 0}>
+            <ExpensiveTree number={0} />
+          </Tab>
+          <Tab hidden={active !== 1}>
+            <PokemonTree />
+          </Tab>
+          <Tab hidden={active !== 2}>
+            <ExpensiveTree number={2} />
+          </Tab>
+        </React.Suspense>
       </Grid>
     </>
   );
---
+++ src/containers/Expensive.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Expensive({ number }) {
+  return <div>I am Expensive {number}</div>;
+}
+
+export default Expensive;
